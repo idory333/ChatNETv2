7eb8d2e112e22223481f0c57b3eb21b9a259d97e
idory333 <168299251+idory333@users.noreply.github.com>
2025-11-30 17:55:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
+
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
